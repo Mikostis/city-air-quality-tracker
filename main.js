7c737b048e8f6e0d8748b0b21dfc3ecdf0e368e1
Mixalis Kostis <kostismichalisuth@gmail.com>
2023-09-18 20:09:35 +0300

    added .env and error handling

--- main.js
+++ main.js
@@ -4,12 +4,9 @@ const app = express();
 const port = 3000;
 const cron = require("node-cron");
 const fetch = require("node-fetch");
-require("dotenv").config();
 
 // database connection
 const db = require("./database");
-
-const apiKey = process.env.API_KEY;
 
 /**
  * axios Furth
@@ -17,6 +14,8 @@ const apiKey = process.env.API_KEY;
 
 // Define an API endpoint to get air quality of the nearest city using Axios
 app.get("/air-quality-nearest-city-axios", async (req, res) => {
+  const apiKey = "0d132a15-ab92-4b8b-8ff6-45d883a8b1f5"; // Replace with your actual API key
+
   try {
     // Make a request to the IQAir API using Axios to get air quality
     const response = await axios.get(
@@ -34,6 +33,8 @@ app.get("/air-quality-nearest-city-axios", async (req, res) => {
 
 // Define an API endpoint to get air quality for Furth using Axios
 app.get("/air-quality-furth-axios", async (req, res) => {
+  const apiKey = "0d132a15-ab92-4b8b-8ff6-45d883a8b1f5"; // Replace with your actual API key
+
   try {
     // Make a request to the IQAir API using Axios to get air quality for Furth
     const response = await axios.get(
@@ -83,12 +84,14 @@ app.get("/most-polluted-day", async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Unable to retrieve most polluted day" });
+    res.status(500).json({ error: "Database error: " + error.message });
   }
 });
 
 // CRON job to fetch and save air quality data for Nuremberg-Furth every 1 minute
 cron.schedule("* * * * *", async () => {
+  const apiKey = "0d132a15-ab92-4b8b-8ff6-45d883a8b1f5"; // Replace with your actual API key
+
   try {
     // Make a request to the IQAir API using Axios to get air quality for Nuremberg
 
@@ -116,6 +119,8 @@ cron.schedule("* * * * *", async () => {
  */
 
 app.get("/air-quality-munich-fetch", async (req, res) => {
+  const apiKey = "0d132a15-ab92-4b8b-8ff6-45d883a8b1f5";
+
   try {
     // Make a request to the IQAir API using Fetch to get air quality for Munich
     const response = await fetch(
@@ -170,37 +175,13 @@ app.get("/most-polluted-day-munich", async (req, res) => {
     }
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .json({ error: "Unable to retrieve most polluted day for Munich" });
+    res.status(500).json({ error: "Database error: " + error.message });
   }
 });
 
-// CRON job to fetch and save air quality data for Munich using Fetch
-cron.schedule("* * * * *", async () => {
-  try {
-    // Make a request to the IQAir API using Fetch to get air quality for Munich
-    const response = await fetch(
-      `http://api.airvisual.com/v2/city?city=Munich&state=Bavaria&country=Germany&key=${apiKey}`
-    );
-
-    if (!response.ok) {
-      throw new Error("Unable to fetch air quality data for Munich");
-    }
-
-    const airQualityData = await response.json();
-
-    // Save air quality data and timestamp to the database for Munich
-    const client = await db.connect(); // Assuming you've defined 'db' as your database connection
-    const query =
-      "INSERT INTO air_quality (data, timestamp) VALUES ($1, NOW())";
-    await client.query(query, [JSON.stringify(airQualityData)]);
-    client.release();
-
-    console.log("Data fetched and saved for Munich : ", airQualityData);
-  } catch (error) {
-    console.error("CRON job error:", error);
-  }
+// Handle other routes and start the server
+app.get("*", (req, res) => {
+  res.status(404).json({ error: "Route not found" });
 });
 
 app.listen(port, () => {
